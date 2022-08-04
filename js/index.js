733558d32a7113436f5df26ac98be9e83110f83c
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-04 15:19:36 -0500

    players

--- js/index.js
+++ js/index.js
@@ -1,29 +1,22 @@
 const selectPlayer = document.querySelectorAll('.container-player')
 const selectImage = document.querySelectorAll('.container-player .fig .img')
-localStorage.setItem("selectPlayers", "o")
 
-
-
+const resetPlayer = () => {
+    selectPlayer[0].classList.remove('container-player__select')
+    selectPlayer[1].classList.remove('container-player__select')
+    selectImage[0].setAttribute("src", "../assets/select-white-x.svg")
+    selectImage[1].setAttribute("src", "../assets/select-white-o.svg")
+}
 
 for (let i = 0 ;i < selectPlayer.length ; i ++ ){
     selectPlayer[i].addEventListener("click" , (e) => {
-        selectPlayer[0].classList.remove('container-player__select')
-        selectPlayer[1].classList.remove('container-player__select')
-        selectImage[0].setAttribute("src", "../assets/select-white-x.svg")
-        selectImage[1].setAttribute("src", "../assets/select-white-o.svg")
-
+        resetPlayer()
         e.target.classList.add('container-player__select')
         if(e.target.classList[1] === "x" ){
             selectImage[0].setAttribute("src", "../assets/select-blue-x.svg")
         }else{
             selectImage[1].setAttribute("src", "../assets/select-blue-o.svg")
+
         }
     })
 }
-
-
-
-
-
-
-
