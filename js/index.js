4e4e94bc7e52d9d87700db638c4d5e8309646607
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-04 10:52:08 -0500

    selcet-players

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
+const selectPlayer = document.querySelectorAll('.container-player')
+const selectImage = document.querySelectorAll('.container-player .fig .img')
+localStorage.setItem("selectPlayers", "o")
+
+
+
+
+for (let i = 0 ;i < selectPlayer.length ; i ++ ){
+    selectPlayer[i].addEventListener("click" , (e) => {
+        selectPlayer[0].classList.remove('container-player__select')
+        selectPlayer[1].classList.remove('container-player__select')
+        selectImage[0].setAttribute("src", "../assets/select-white-x.svg")
+        selectImage[1].setAttribute("src", "../assets/select-white-o.svg")
+
+        e.target.classList.add('container-player__select')
+        if(e.target.classList[1] === "x" ){
+            selectImage[0].setAttribute("src", "../assets/select-blue-x.svg")
+        }else{
+            selectImage[1].setAttribute("src", "../assets/select-blue-o.svg")
+        }
+    })
+}
+
+
+
+
+
 
 
